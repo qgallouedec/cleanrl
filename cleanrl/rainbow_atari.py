fab006d653f4a613af30f20f4ad1f24036690f82
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2023-11-19 09:03:45 +0100

    rename in push to hub

--- cleanrl/rainbow_atari.py
+++ cleanrl/rainbow_atari.py
@@ -339,7 +339,7 @@ poetry run pip install "stable_baselines3==2.0.0a1" "gymnasium[atari,accept-rom-
 
             repo_name = f"{args.env_id}-{args.exp_name}-seed{args.seed}"
             repo_id = f"{args.hf_entity}/{repo_name}" if args.hf_entity else repo_name
-            push_to_hub(args, episodic_returns, repo_id, "C51", f"runs/{run_name}", f"videos/{run_name}-eval")
+            push_to_hub(args, episodic_returns, repo_id, "RAINBOW", f"runs/{run_name}", f"videos/{run_name}-eval")
 
     envs.close()
     writer.close()
